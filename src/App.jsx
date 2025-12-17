0c07b2460950643cea84560a657af33de8093a6f
Cokumu Junior <cokumujnr@gmail.com>
2025-12-17 19:44:37 +0300

    hero first part complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 
 import gsap from "gsap"
 import { ScrollTrigger, SplitText } from "gsap/all"
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex items-center bg-amber-600">
-      <h1>hello 2026</h1>
-    </div>
+
+    <main>
+      <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black" />
+    </main>
+
   )
 }
 
